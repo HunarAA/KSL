29c29efa69234c983fab00576d0aba20631ef970
Hunar <honar.cs@gmail.com>
2022-05-16 11:37:43 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,30 @@
+from streamlit_webrtc import ClientSettings
+
+# CLASSES = [
+#     'Ruberru',
+#     'Choni',
+#     'Leberdem',
+#     'Xoshewisti',
+#     'Cheshtxane',
+#     'zherpyalle',
+#     'Xorragir',
+#     'Sistem',
+#     'Tebayi',
+#     'Rengekan']
+CLASSES = [
+    'ڕوبەڕوو',
+    'چۆنی',
+    'لەبەردەم',
+    'خۆشەویستی',
+    'چێشتخانە',
+    'ژێرپیاڵە',
+    'خۆڕاگر',
+    'سیستەم',
+    'تەبایی',
+    'ڕەنگەکان']
+
+
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": True, "audio": False},
+    )
